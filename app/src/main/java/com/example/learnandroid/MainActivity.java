a127e2f6796c37e978b5c5c28b7a7c4455ad3df4
zhujuntao <juntao.zhu@pactera.com>
2020-06-10 10:33:10 +0800

    提交一次更新

--- app/src/main/java/com/example/learnandroid/MainActivity.java
+++ app/src/main/java/com/example/learnandroid/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //提交dev
         setContentView(R.layout.activity_main);
         findViewById(R.id.tv_jump).setOnClickListener(new View.OnClickListener() {
             @Override
